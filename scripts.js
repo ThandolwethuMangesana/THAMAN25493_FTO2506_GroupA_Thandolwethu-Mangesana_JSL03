467eddbbdea53b303000747275ec17d0f08b7fda
Thandolwethu Mangesana <Mangesanathandolwethu@gmail.com>
2025-09-05 02:08:08 +0200

    update scripts.js added commenting and more initial task

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+//arat to store initial tasks
+// Each task has an id, title, description, and status (todo, doing, done)
+//added 3 more tasks
+
 const initialTasks = [
   {
     id: 1,
@@ -17,5 +21,23 @@ const initialTasks = [
     description:
       "Gain practical experience and collaborate with others in the software development community",
     status: "done",
+  },
+  {
+    id: 4,
+    title: "Correct JS02",
+    description: "Correct JSL02 before working on JS03",
+    status: "done",
+  },
+  {
+    id: 5,
+    title: "Start JS03",
+    description: "Start working on JS03 after correcting JS02",
+    status: "todo",
+  },
+  {
+    id: 6,
+    title: "Practice Coding Challenges",
+    description: "practice coding challenges on LMS to guage my understanding",
+    status: "doing",
   },
 ];

bf87acfcd19a80010be11d8629ae8c0ba73348f0
Thandolwethu Mangesana <Mangesanathandolwethu@gmail.com>
2025-09-06 17:54:05 +0200

    update script.js added a new task, push, length of tasks, alerts

--- scripts.js
+++ scripts.js
@@ -40,3 +40,26 @@ function addTask() {
   alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
     status = prompt("Invalid status. Please enter 'todo', 'doing', or 'done':").toLowerCase();
 }
+
+//creates a new task object and adds it to the initialTasks array
+const newTask = {
+  id: initialTasks[initialTasks.length - 1].id + 1,
+  title: title,
+  description: description,
+  status: status,
+};
+
+//adds the new task to the array
+initialTasks.push(newTask);
+
+//number of tasks added
+while (initialTasks.length < 6) {
+  addTask();
+}
+
+// alerts the user if adds more than required tasks
+if (initialTasks.length === 6) {
+  alert("There are enough tasks on your board, please check them in the console")
+}
+
+
